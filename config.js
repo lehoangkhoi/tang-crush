4b47063718891ae17f6975eaf970edf804b34b2f
lehoangkhoi <86224383+lehoangkhoi@users.noreply.github.com>
2021-06-21 15:20:19 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Thủy ơi!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Thời gian qua em vất vả nhiều rồi. Giờ anh đã thật sự thay đổi anh không cư xử như trước nữa đâu. Cho anh cơ hội nắm tay em bước tiếp trên con đườn phía trước nha',
+    btnAccept: 'dạ!!! nhưng hứa với em kh làm em khóc nữa là được hihi^^ <3',
+    faceLink: 'https://www.facebook.com/le.h.khoi.3
+}
